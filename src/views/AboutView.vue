7fb42fab1229bc393e21d0e1fdefa0666af89796
xiaohe <1794713974@qq.com>
2023-09-14 17:56:26 +0800

    chore: 引入组件库 naive-ui

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="about">
     <p class="text-3xl bg-red-100">hello tailwindcss</p>
+    <NButton>hello naive-ui</NButton>
   </div>
 </template>
+
+<script setup lang="ts">
+import { NButton } from 'naive-ui'
+</script>
 
 <style>
 @media (min-width: 1024px) {
